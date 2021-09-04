c86e798a4f8740f236d0cf4a16cdac79a2cda0f8
maryamalrubaye <maryamalrubaye18@gmail.com>
2021-09-04 12:49:27 +0300

    adding async to make the code more readable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { useState }  from 'react';
+import React, { useState } from "react";
 
-
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const [movies,setMovies]=useState([])
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films').then((response) => {return response.json()}).then((data) => {
-      const translatedMovies = data.results.map(movieDate=>{
-        return{
-          id : movieDate.episode_id,
-          title : movieDate.title,
-          openingText : movieDate.opening_crawl,
-          releaseDate : movieDate.release_date
-
-        }
-      });
-      setMovies(translatedMovies)
-    })
-  };
+  const [movies, setMovies] = useState([]);
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+    const translatedMovies = data.results.map((movieDate) => {
+      return {
+        id: movieDate.episode_id,
+        title: movieDate.title,
+        openingText: movieDate.opening_crawl,
+        releaseDate: movieDate.release_date,
+      };
+    });
+    setMovies(translatedMovies);
+  }
 
   return (
     <React.Fragment>
